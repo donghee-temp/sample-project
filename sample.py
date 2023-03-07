d596ced8a1acd799952e06d28507edd1c5e455ae
donghee-temp <100888989+donghee-temp@users.noreply.github.com>
2023-03-08 00:38:16 +0900

    CI Test (#4)

--- sample.py
+++ sample.py
@@ -1,6 +1,12 @@
 def fibonacci(n: int) -> int:
-    raise NotImplementedError("Should be implemented")
+    if n == 0:
+        return 0
+    if n in (1, 2):
+        return 1
+    return fibonacci(n-1) + fibonacci(n-2)
 
 
 def factorial(n: int) -> int:
-    raise NotImplementedError("Should be implemented")
+    if n <= 1:
+        return 1
+    return n * factorial(n-1)
